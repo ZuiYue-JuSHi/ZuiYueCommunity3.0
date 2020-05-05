32507de16363b80e467a965468059a8d40aaf36e
ZuiYue <ZuiYue7463@outlook.com>
2020-05-05 11:52:37 +0800

    firsht

---
+++ src/main/java/com/zuiyue/community/ZuiyuecommunityApplication.java
@@ -0,0 +1,13 @@
+package com.zuiyue.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuiyuecommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuiyuecommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zuiyue/community/ZuiyuecommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuiyue.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuiyuecommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
